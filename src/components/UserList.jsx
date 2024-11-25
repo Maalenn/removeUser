fac45f24e2d7bd5a92c0851555f230990631d4e2
Khun Malen <malen8622@gmail.com>
2024-11-25 18:39:01 +0700

    work with reducer

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,23 +1,47 @@
 import userData from "../data.js";
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const initialState = {
+  user: userData,
+};
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, user: [] };
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, user: userData };
+  }
+  if (action.type === REMOVE_ITEM) {
+    const filterUser = state.user.filter((pp) => pp.id !== action.payload.id);
+    return { ...state, user: filterUser };
+  }
+  throw new Error(`CAN"T FIND ${action.type}`);
+};
 
 const UserList = () => {
-  const [user, setUser] = useState(userData);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleClear = () => setUser([]);
-
-  const handleRemove = (id) => {
-    const filterUser = user.filter((pp) => pp.id !== id);
-    setUser(filterUser);
+  const handleClear = () => {
+    dispatch({ type: CLEAR_LIST });
   };
 
   const handleReset = () => {
-    setUser(userData);
+    dispatch({ type: RESET_LIST });
   };
+  
+  const handleRemove = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
+  };
+
 
   return (
     <div className="container">
-      {user.map(({ id, img, name, age }) => (
+      {state.user.map(({ id, img, name, age }) => (
         <article key={id}>
           <img src={img} alt="" />
           <h2>{name}</h2>
@@ -25,7 +49,7 @@ const UserList = () => {
           <button onClick={() => handleRemove(id)}>Remove</button>
         </article>
       ))}
-      {user.length === 0 ? (
+      {state.user.length === 0 ? (
         <button onClick={handleReset}>Reset All</button>
       ) : (
         <button onClick={handleClear}>Clear All</button>
